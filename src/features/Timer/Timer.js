407683bd0597b88af190dc7581274193838d19f3
“Pascwin” <pascal-winner@web.de>
2022-03-03 15:45:54 +0100

    added Timecontrol

--- src/features/Timer/Timer.js
+++ src/features/Timer/Timer.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import Countdown from "../../components/Countdown";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Vibration } from "react-native";
 import { ProgressBar } from "react-native-paper";
 import { colors } from "../../utils/colors";
 import RoundedButton from "../../components/RoundedButton"
 import { spacing } from "../../utils/sizes";
 
 const DEFAULT_TIME = 0.1;
+
+const ONE_SECOND_IN_MS = 1000;
+
+const PATTERN = [
+    1 * ONE_SECOND_IN_MS,
+    2 * ONE_SECOND_IN_MS,
+    3 * ONE_SECOND_IN_MS
+];
 
 export const Timer = ({ focusSubject, onTimerEnd, clearSubject }) => {
 
@@ -19,15 +27,24 @@ export const Timer = ({ focusSubject, onTimerEnd, clearSubject }) => {
             <View style={styles.container}>
 
                 <View style={styles.countdown}>
-                    <Countdown isPaused={!isStarted} onProgress={setProgress} minutes={minutes} onEnd={() => { }} />
+                    <Countdown isPaused={!isStarted} onProgress={setProgress} minutes={minutes}
+                        onEnd={() => {
+                            Vibration.vibrate(PATTERN)
+                        }}
+                    />
 
                     <View style={{ paddingTop: spacing.md, alignItems: "center" }}>
                         <Text style={styles.title}>Focus on {focusSubject}</Text>
                         <Text style={styles.task}>Focus on {focusSubject}</Text>
                     </View>
                 </View>
-                <View style={{paddingTop: spacing.sm}}>
-                    <ProgressBar color={colors.progressBar} style={{height: 10}} progress={progress} />
+                <View style={{ paddingTop: spacing.sm }}>
+                    <ProgressBar color={colors.progressBar} style={{ height: 10 }} progress={progress} />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <RoundedButton title="10" size={75} onPress={() => setMinutes(10)} style={{margin: 5}}/>
+                    <RoundedButton title="15" size={75} onPress={() => setMinutes(15)} style={{margin: 5}}/>
+                    <RoundedButton title="20" size={75} onPress={() => setMinutes(20)} style={{margin: 5}}/>
                 </View>
                 <View style={styles.buttonWrapper}>
                     {!isStarted ?
@@ -65,5 +82,11 @@ const styles = StyleSheet.create({
         padding: 15,
         justifyContent: "center",
         alignItems: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginTop: spacing.lg,
+        alignItems: "center",
+        justifyContent: "center"
     }
 })
